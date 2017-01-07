5042e09201feb2d1f082c9e9a0070f51984222cf
Andrey Sitnik <andrey@sitnik.ru>
2017-01-07 11:22:56 +1000

    Add stash note to prompt

--- prompt/main.zsh
+++ prompt/main.zsh
@@ -19,18 +19,34 @@
 # \e[K  => clears everything after the cursor on the current line
 # \e[2K => clear everything on the current line
 
-prompt_pure_clear_screen() {
+prompt_ai_clear_screen() {
   # enable output to terminal
   zle -I
   # clear screen and move cursor to (0, 0)
   print -n '\e[2J\e[0;0H'
   # print preprompt
-  prompt_pure_preprompt_render precmd
+  prompt_ai_preprompt_render precmd
 }
 
-prompt_pure_check_git_arrows() {
+prompt_ai_check_git_stash() {
   # reset git arrows
-  prompt_pure_git_arrows=
+  prompt_ai_git_stash=
+
+  local stash_count
+
+  # check git
+  stash_count="$(command git stash list 2>/dev/null | wc -l)"
+  # exit if the command failed
+  (( !$? )) || return
+
+  if [ "$stash_count" != "0" ]; then
+    prompt_ai_git_stash=" (stash $stash_count)"
+  fi
+}
+
+prompt_ai_check_git_arrows() {
+  # reset git arrows
+  prompt_ai_git_arrows=
 
   # check if there is an upstream configured for this branch
   command git rev-parse --abbrev-ref @'{u}' &>/dev/null || return
@@ -48,10 +64,10 @@ prompt_pure_check_git_arrows() {
   (( ${right:-0} > 0 )) && arrows+="↓"
   (( ${left:-0} > 0 )) && arrows+="↑"
 
-  [[ -n $arrows ]] && prompt_pure_git_arrows=" ${arrows}"
+  [[ -n $arrows ]] && prompt_ai_git_arrows=" ${arrows}"
 }
 
-prompt_pure_set_title() {
+prompt_ai_set_title() {
   # emacs terminal does not support settings the title
   (( ${+EMACS} )) && return
 
@@ -69,18 +85,18 @@ prompt_pure_set_title() {
   print -n '\a'
 }
 
-prompt_pure_preexec() {
-  # attempt to detect and prevent prompt_pure_async_git_fetch from interfering with user initiated git or hub fetch
-  [[ $2 =~ (git|hub)\ .*(pull|fetch) ]] && async_flush_jobs 'prompt_pure'
+prompt_ai_preexec() {
+  # attempt to detect and prevent prompt_ai_async_git_fetch from interfering with user initiated git or hub fetch
+  [[ $2 =~ (git|hub)\ .*(pull|fetch) ]] && async_flush_jobs 'prompt_ai'
 
-  prompt_pure_cmd_timestamp=$EPOCHSECONDS
+  prompt_ai_cmd_timestamp=$EPOCHSECONDS
 
   # shows the current dir and executed command in the title while a process is active
-  prompt_pure_set_title 'ignore-escape' "$PWD:t: $2"
+  prompt_ai_set_title 'ignore-escape' "$PWD:t: $2"
 }
 
 # string length ignoring ansi escapes
-prompt_pure_string_length_to_var() {
+prompt_ai_string_length_to_var() {
   local str=$1 var=$2 length
   # perform expansion on str and check length
   length=$(( ${#${(S%%)str//(\%([KF1]|)\{*\}|\%[Bbkf])}} ))
@@ -89,7 +105,7 @@ prompt_pure_string_length_to_var() {
   typeset -g "${var}"="${length}"
 }
 
-prompt_pure_preprompt_render() {
+prompt_ai_preprompt_render() {
   # store the current prompt_subst setting so that it can be restored later
   local prompt_subst_status=$options[prompt_subst]
 
@@ -103,37 +119,40 @@ prompt_pure_preprompt_render() {
   local preprompt=""
 
   # username and machine if applicable
-  preprompt+=$prompt_pure_username
+  preprompt+=$prompt_ai_username
   # current dir
   preprompt+="%F{green}$prompt_dir"
   # git info
   if [ "$vcs_info_msg_0_" != ' master' ]; then
     preprompt+=$vcs_info_msg_0_
   fi
-  preprompt+=$prompt_pure_git_dirty
+  # git stash
+  preprompt+="%F{yellow}${prompt_ai_git_stash}%f"
+  # git dirtys
+  preprompt+=$prompt_ai_git_dirty
   # git pull/push arrows
-  preprompt+="%F{yellow}${prompt_pure_git_arrows}%f"
+  preprompt+="%F{yellow}${prompt_ai_git_arrows}%f"
 
-  # make sure prompt_pure_last_preprompt is a global array
-  typeset -g -a prompt_pure_last_preprompt
+  # make sure prompt_ai_last_preprompt is a global array
+  typeset -g -a prompt_ai_last_preprompt
 
   # if executing through precmd, do not perform fancy terminal editing
   if [[ "$1" == "precmd" ]]; then
     print -P "\n${preprompt}"
   else
     # only redraw if the expanded preprompt has changed
-    [[ "${prompt_pure_last_preprompt[2]}" != "${(S%%)preprompt}" ]] || return
+    [[ "${prompt_ai_last_preprompt[2]}" != "${(S%%)preprompt}" ]] || return
 
     # calculate length of preprompt and store it locally in preprompt_length
     integer preprompt_length lines
-    prompt_pure_string_length_to_var "${preprompt}" "preprompt_length"
+    prompt_ai_string_length_to_var "${preprompt}" "preprompt_length"
 
     # calculate number of preprompt lines for redraw purposes
     (( lines = ( preprompt_length - 1 ) / COLUMNS + 1 ))
 
     # calculate previous preprompt lines to figure out how the new preprompt should behave
     integer last_preprompt_length last_lines
-    prompt_pure_string_length_to_var "${prompt_pure_last_preprompt[1]}" "last_preprompt_length"
+    prompt_ai_string_length_to_var "${prompt_ai_last_preprompt[1]}" "last_preprompt_length"
     (( last_lines = ( last_preprompt_length - 1 ) / COLUMNS + 1 ))
 
     # clr_prev_preprompt erases visual artifacts from previous preprompt
@@ -172,31 +191,34 @@ prompt_pure_preprompt_render() {
   fi
 
   # store both unexpanded and expanded preprompt for comparison
-  prompt_pure_last_preprompt=("$preprompt" "${(S%%)preprompt}")
+  prompt_ai_last_preprompt=("$preprompt" "${(S%%)preprompt}")
 }
 
-prompt_pure_precmd() {
+prompt_ai_precmd() {
   # check for git arrows
-  prompt_pure_check_git_arrows
+  prompt_ai_check_git_arrows
+
+  # check for git stash
+  prompt_ai_check_git_stash
 
   # shows the full path in the title
-  prompt_pure_set_title 'expand-prompt' '%~'
+  prompt_ai_set_title 'expand-prompt' '%~'
 
   # get vcs info
   vcs_info
 
   # preform async git dirty check and fetch
-  prompt_pure_async_tasks
+  prompt_ai_async_tasks
 
   # print the preprompt
-  prompt_pure_preprompt_render "precmd"
+  prompt_ai_preprompt_render "precmd"
 
-  # remove the prompt_pure_cmd_timestamp, indicating that precmd has completed
-  unset prompt_pure_cmd_timestamp
+  # remove the prompt_ai_cmd_timestamp, indicating that precmd has completed
+  unset prompt_ai_cmd_timestamp
 }
 
 # fastest possible way to check if repo is dirty
-prompt_pure_async_git_dirty() {
+prompt_ai_async_git_dirty() {
   local untracked_dirty=$1; shift
 
   # use cd -q to avoid side effects of changing directory, e.g. chpwd hooks
@@ -211,7 +233,7 @@ prompt_pure_async_git_dirty() {
   (( $? )) && echo " %F{yellow}⚡%f"
 }
 
-prompt_pure_async_git_fetch() {
+prompt_ai_async_git_fetch() {
   # use cd -q to avoid side effects of changing directory, e.g. chpwd hooks
   builtin cd -q "$*"
 
@@ -219,28 +241,28 @@ prompt_pure_async_git_fetch() {
   GIT_TERMINAL_PROMPT=0 command git -c gc.auto=0 fetch
 }
 
-prompt_pure_async_tasks() {
+prompt_ai_async_tasks() {
   # initialize async worker
-  ((!${prompt_pure_async_init:-0})) && {
-    async_start_worker "prompt_pure" -u -n
-    async_register_callback "prompt_pure" prompt_pure_async_callback
-    prompt_pure_async_init=1
+  ((!${prompt_ai_async_init:-0})) && {
+    async_start_worker "prompt_ai" -u -n
+    async_register_callback "prompt_ai" prompt_ai_async_callback
+    prompt_ai_async_init=1
   }
 
   # store working_tree without the "x" prefix
   local working_tree="${vcs_info_msg_1_#x}"
 
-  # check if the working tree changed (prompt_pure_current_working_tree is prefixed by "x")
-  if [[ ${prompt_pure_current_working_tree#x} != $working_tree ]]; then
+  # check if the working tree changed (prompt_ai_current_working_tree is prefixed by "x")
+  if [[ ${prompt_ai_current_working_tree#x} != $working_tree ]]; then
     # stop any running async jobs
-    async_flush_jobs "prompt_pure"
+    async_flush_jobs "prompt_ai"
 
     # reset git preprompt variables, switching working tree
-    unset prompt_pure_git_dirty
-    unset prompt_pure_git_last_dirty_check_timestamp
+    unset prompt_ai_git_dirty
+    unset prompt_ai_git_last_dirty_check_timestamp
 
     # set the new working tree and prefix with "x" to prevent the creation of a named path by AUTO_NAME_DIRS
-    prompt_pure_current_working_tree="x${working_tree}"
+    prompt_ai_current_working_tree="x${working_tree}"
   fi
 
   # only perform tasks inside git working tree
@@ -249,41 +271,42 @@ prompt_pure_async_tasks() {
   # do not preform git fetch if it is disabled or working_tree == HOME
   if (( ${PURE_GIT_PULL:-1} )) && [[ $working_tree != $HOME ]]; then
     # tell worker to do a git fetch
-    async_job "prompt_pure" prompt_pure_async_git_fetch "${working_tree}"
+    async_job "prompt_ai" prompt_ai_async_git_fetch "${working_tree}"
   fi
 
   # if dirty checking is sufficiently fast, tell worker to check it again, or wait for timeout
-  integer time_since_last_dirty_check=$(( EPOCHSECONDS - ${prompt_pure_git_last_dirty_check_timestamp:-0} ))
+  integer time_since_last_dirty_check=$(( EPOCHSECONDS - ${prompt_ai_git_last_dirty_check_timestamp:-0} ))
   if (( time_since_last_dirty_check > ${PURE_GIT_DELAY_DIRTY_CHECK:-1800} )); then
-    unset prompt_pure_git_last_dirty_check_timestamp
+    unset prompt_ai_git_last_dirty_check_timestamp
     # check check if there is anything to pull
-    async_job "prompt_pure" prompt_pure_async_git_dirty "${PURE_GIT_UNTRACKED_DIRTY:-1}" "${working_tree}"
+    async_job "prompt_ai" prompt_ai_async_git_dirty "${PURE_GIT_UNTRACKED_DIRTY:-1}" "${working_tree}"
   fi
 }
 
-prompt_pure_async_callback() {
+prompt_ai_async_callback() {
   local job=$1
   local output=$3
   local exec_time=$4
 
   case "${job}" in
-    prompt_pure_async_git_dirty)
-      prompt_pure_git_dirty=$output
-      prompt_pure_preprompt_render
+    prompt_ai_async_git_dirty)
+      prompt_ai_git_dirty=$output
+      prompt_ai_preprompt_render
 
-      # When prompt_pure_git_last_dirty_check_timestamp is set, the git info is displayed in a different color.
+      # When prompt_ai_git_last_dirty_check_timestamp is set, the git info is displayed in a different color.
       # To distinguish between a "fresh" and a "cached" result, the preprompt is rendered before setting this
       # variable. Thus, only upon next rendering of the preprompt will the result appear in a different color.
-      (( $exec_time > 2 )) && prompt_pure_git_last_dirty_check_timestamp=$EPOCHSECONDS
+      (( $exec_time > 2 )) && prompt_ai_git_last_dirty_check_timestamp=$EPOCHSECONDS
       ;;
-    prompt_pure_async_git_fetch)
-      prompt_pure_check_git_arrows
-      prompt_pure_preprompt_render
+    prompt_ai_async_git_fetch)
+      prompt_ai_check_git_arrows
+      prompt_ai_check_git_stash
+      prompt_ai_preprompt_render
       ;;
   esac
 }
 
-prompt_pure_setup() {
+prompt_ai_setup() {
   # prevent percentage showing up
   # if output doesn't end with a newline
   export PROMPT_EOL_MARK=''
@@ -298,8 +321,8 @@ prompt_pure_setup() {
   autoload -Uz vcs_info
   autoload -Uz async && async
 
-  add-zsh-hook precmd prompt_pure_precmd
-  add-zsh-hook preexec prompt_pure_preexec
+  add-zsh-hook precmd prompt_ai_precmd
+  add-zsh-hook preexec prompt_ai_preexec
 
   zstyle ':vcs_info:*' enable git
   zstyle ':vcs_info:*' use-simple true
@@ -314,17 +337,17 @@ prompt_pure_setup() {
   # override the builtin one so that the preprompt is displayed correctly when
   # ^L is issued.
   if [[ $widgets[clear-screen] == 'builtin' ]]; then
-    zle -N clear-screen prompt_pure_clear_screen
+    zle -N clear-screen prompt_ai_clear_screen
   fi
 
   # show username@host if logged in through SSH
-  [[ "$SSH_CONNECTION" != '' ]] && prompt_pure_username='%F{242}%n@%m%f '
+  [[ "$SSH_CONNECTION" != '' ]] && prompt_ai_username='%F{242}%n@%m%f '
 
   # show username if root, with username in white
-  [[ $UID -eq 0 ]] && prompt_pure_username='%F{white}%B%n%b%f '
+  [[ $UID -eq 0 ]] && prompt_ai_username='%F{white}%B%n%b%f '
 
   # prompt turns red if the previous command didn't exit with 0
   PROMPT="%F{green}➤%f "
 }
 
-prompt_pure_setup "$@"
+prompt_ai_setup "$@"
